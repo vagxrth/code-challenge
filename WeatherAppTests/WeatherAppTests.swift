58b952bd36a73ede6fe00585d926d86f62646774
Vagarth <vagarth419@gmail.com>
2024-11-12 18:41:47 +0530

    Initial Commit

---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Vagarth Pandey on 12/11/24.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
