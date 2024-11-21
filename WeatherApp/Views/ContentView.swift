801b1a81ede8ae40ba07508232486845681f1d6d
Vagarth <vagarth419@gmail.com>
2024-11-21 18:13:30 +0530

    feat: modify the content view to intialize location manager

--- WeatherApp/Views/ContentView.swift
+++ WeatherApp/Views/ContentView.swift
@@ -8,14 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello, World!")
+            .padding()
     }
 }
 
